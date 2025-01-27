40ba32b6eda61e42609f29a06f328f19d8b7ba0a
Qin Jiangban <tanjiangban@gmail.com>
2025-01-27 15:04:32 +0800

    更新 next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* output: 'export' */
+  output: "export", // 启用静态导出
+  basePath: "/labs.coolha.com", // 替换为仓库名称，例如 "/docs.coolha.com"
+  images: {
+    unoptimized: true, // 禁用服务端优化
+  },
 }
 
 module.exports = withNextIntl(nextConfig)
